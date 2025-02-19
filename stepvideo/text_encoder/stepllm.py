78abb87ea2b8bfcf62a99fad04e12cda361c9e5a
zhanghuiyao <1814619459@qq.com>
2025-02-19 16:24:25 +0800

    fix

--- stepvideo/text_encoder/stepllm.py
+++ stepvideo/text_encoder/stepllm.py
@@ -322,25 +322,7 @@ class STEP1TextEncoder(nn.Cell):
         return ops.stop_gradient(y.transpose(0,1)), ops.stop_gradient(y_mask)
 
 
-    def to(self, dtype):
-        module_names, _ = self._get_signature_keys(self)
-        modules = [getattr(self, n, None) for n in module_names]
-        modules = [m for m in modules if isinstance(m, nn.Cell)]
-        for module in modules:
-            module.to(dtype)
+    def to(self, dtype: Optional[ms.Type] = None):
+        for p in self.get_parameters():
+            p.set_dtype(dtype)
         return self
-
-    @classmethod
-    def _get_signature_keys(cls, obj):
-        parameters = inspect.signature(obj.__init__).parameters
-        required_parameters = {k: v for k, v in parameters.items() if v.default == inspect._empty}
-        optional_parameters = set({k for k, v in parameters.items() if v.default != inspect._empty})
-        expected_modules = set(required_parameters.keys()) - {"self"}
-
-        optional_names = list(optional_parameters)
-        for name in optional_names:
-            if name in cls._optional_components:
-                expected_modules.add(name)
-                optional_parameters.remove(name)
-
-        return expected_modules, optional_parameters
--- stepvideo/vae/vae.py
+++ stepvideo/vae/vae.py
@@ -12,6 +12,8 @@
 # ==============================================================================
 
 import inspect
+from typing import Optional
+
 import mindspore as ms
 from mindspore import nn, ops, Tensor, Parameter
 from mindspore.communication.management import get_group_size, get_rank
@@ -1129,25 +1131,7 @@ class AutoencoderKL(nn.Cell):
         return x
 
 
-    def to(self, dtype):
-        module_names, _ = self._get_signature_keys(self)
-        modules = [getattr(self, n, None) for n in module_names]
-        modules = [m for m in modules if isinstance(m, nn.Cell)]
-        for module in modules:
-            module.to(dtype)
+    def to(self, dtype: Optional[ms.Type] = None):
+        for p in self.get_parameters():
+            p.set_dtype(dtype)
         return self
-
-    @classmethod
-    def _get_signature_keys(cls, obj):
-        parameters = inspect.signature(obj.__init__).parameters
-        required_parameters = {k: v for k, v in parameters.items() if v.default == inspect._empty}
-        optional_parameters = set({k for k, v in parameters.items() if v.default != inspect._empty})
-        expected_modules = set(required_parameters.keys()) - {"self"}
-
-        optional_names = list(optional_parameters)
-        for name in optional_names:
-            if name in cls._optional_components:
-                expected_modules.add(name)
-                optional_parameters.remove(name)
-
-        return expected_modules, optional_parameters
