a58ddd6fbe51a25a62e89aedad161890fee54c5f
zhanghuiyao <1814619459@qq.com>
2025-02-21 22:13:25 +0800

    1

--- test_llm.py
+++ test_llm.py
@@ -17,6 +17,7 @@ from api.call_remote_server import CaptionPipeline, StepVaePipeline
 if __name__ == "__main__":
     args = parse_args()
     args.llm_dir = "step_llm"
+    args.clip_dir = "hunyuan_clip"
 
     ms.set_context(
         mode=ms.PYNATIVE_MODE,
