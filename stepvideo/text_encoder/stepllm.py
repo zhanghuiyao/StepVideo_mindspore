e50bbb0e2e3c337809c51bb86c4a41058122a467
zhanghuiyao <1814619459@qq.com>
2025-02-21 22:43:41 +0800

    1

--- stepvideo/text_encoder/stepllm.py
+++ stepvideo/text_encoder/stepllm.py
@@ -251,6 +251,8 @@ class Transformer(nn.Cell):
         # if max_seq_len is not None and not isinstance(max_seq_len, Tensor):
         #     max_seq_len = Tensor(max_seq_len, dtype=ms.int32)
 
+        import pdb;pdb.set_trace()
+
         for lid, layer in enumerate(self.layers):
             hidden_states = layer(
                                     hidden_states,
