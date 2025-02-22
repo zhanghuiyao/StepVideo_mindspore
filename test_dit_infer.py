a3916b9b16d192d0f79992328de244d3019d7883
zhanghuiyao <1814619459@qq.com>
2025-02-22 15:34:53 +0800

    1

--- test_dit_infer.py
+++ test_dit_infer.py
@@ -173,11 +173,11 @@ if __name__ == "__main__":
         # numpy save noise predict
         from stepvideo.parallel import get_rank, is_distribute
         if is_distribute():
-            np.save("./noise_pred.npy", noise_pred.to(ms.float32).asnumpy())
-            print("np.save `./noise_pred.npy` success")
-        else:
             np.save(f"./noise_pred_rank{get_rank()}.npy", noise_pred.to(ms.float32).asnumpy())
             print(f"np.save `./noise_pred_rank{get_rank()}.npy` success")
+        else:
+            np.save("./noise_pred.npy", noise_pred.to(ms.float32).asnumpy())
+            print("np.save `./noise_pred.npy` success")
 
 
         # import pdb;pdb.set_trace()
